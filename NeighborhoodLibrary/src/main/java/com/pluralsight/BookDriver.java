6ca45e40cb00d106ab69baee6116c8a87a98b111
Ezra A Williams <ewilliamsdaniel@gmail.com>
2024-04-23 11:03:42 -0400

    4/22/2024

---
+++ NeighborhoodLibrary/src/main/java/com/pluralsight/BookDriver.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class BookDriver {
+    public static void main(String[] args) {
+        Book book = new Book(1, "abc123","Day One",false,"Ezra");
+        Book book2 = new Book(2, "abc456","Day Two",false,"Marva");
+        Book book3 = new Book(3, "abc789","Day Three",false,"Laura");
+        Book book4 = new Book(4, "abc101","Day Four",false,"Caryl");
+    }
+}
